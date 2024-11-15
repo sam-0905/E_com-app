d11e8d9d140fe2cd6b8100a331d0b17e4f39c031
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-16 00:21:32 +0530

    updated search component

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,20 +2,32 @@ import { useState } from "react";
 import data from './utils/Constant';
 
 const Body = () => {
-    // const[searchText , setSearchText] = useState("");
-    // const[filterData ,setFilterData] = useState(data)
+    const[searchText , setSearchText] = useState();
+    const[filterData ,setFilterData] = useState(data);
 
-    // retrun (
-    //     <>  
-    //     <input
-    //     type="text"
-    //     value={searchText}
-    //     onChange={(e)=>e.target.value}
-    //     placeholder="Search for Products, Brands and More"
-    //     ></input>
-    //     <button></button>
-    //     </>
-    // )
+    const filteredData = () => {
+        const filteredSearch = data.filter((data) => {
+            data?.name?.toLowerCase()?.includes?.(searchText?.toLowerCase())
+        })
+        setFilterData(filteredSearch)
+    }
+
+    const changeSearchHandler = () => (event) => setSearchText(event.target.value)
+
+    return (
+        <>  
+            <div>
+        <input
+        type="text"
+        value={searchText}
+        onChange={changeSearchHandler}
+        placeholder="Search for Products, Brands and More"
+        ></input>
+        <button className="button-86" role="button" onClick={filteredData}>Search</button>
+            </div>
+
+        </>
+    )
 }
 
 export default Body;
