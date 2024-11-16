01a5a8e9efe1624d8f8f22fbc823ede1a70eb38a
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-16 20:00:28 +0530

    updated search element and card

--- src/Body.jsx
+++ src/Body.jsx
@@ -6,26 +6,64 @@ const Body = () => {
     const[filterData ,setFilterData] = useState(data);
 
     const filteredData = () => {
-        const filteredSearch = data.filter((data) => {
+        console.log("filter",searchText)
+        const filteredSearch = data.filter((data) => (
             data?.name?.toLowerCase()?.includes?.(searchText?.toLowerCase())
-        })
+        ));
         setFilterData(filteredSearch)
     }
 
-    const changeSearchHandler = () => (event) => setSearchText(event.target.value)
+    const changeSearchHandler = (e) => setSearchText(e.target.value)
 
     return (
         <>  
-            <div>
+        <div className="search-container">
         <input
         type="text"
         value={searchText}
         onChange={changeSearchHandler}
+        className="search-bar"
         placeholder="Search for Products, Brands and More"
         ></input>
         <button className="button-86" role="button" onClick={filteredData}>Search</button>
-            </div>
+        </div>
+        <div className="card" >
+        {filterData.map(
+          ({
+            id,
+            name,
+            image,
+            price,
+            productName,
+            inStock,
+            level,
+            fastDelivery,
+            ratings,
+          }) => (
 
+            <div className="card">
+                <div className="card-container">
+                <div key={id} className="card-img"> <img src={image} width="100%" height="auto" alt={productName} /> </div>
+                    <div className="card-details">
+                    <h3 className="card-title"> {name} </h3>
+                    <div className="price">Rs. {price}</div>
+                    <div className="stock-label">  {inStock && <div className="in-stock"> In Stock </div>}
+                    {!inStock && <div className="out-stock">  Out of Stock </div>}</div>
+                    <div>{level}</div>
+                    <div className="rating fa fa-star" style={{marginTop:"4px"}}>{ratings}</div>
+                     {fastDelivery ? (
+                     <div >  Fast Delivery </div>
+                     ) : (
+                     <div> 3 days minimum </div>
+                     )}
+                    </div>
+                   
+                </div>
+                   
+             </div>                             
+          )
+        )}
+      </div>
         </>
     )
 }
