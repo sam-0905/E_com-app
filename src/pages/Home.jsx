67205ba1f2da56101b4d8a7eecb7798e34c7bea7
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-20 19:58:26 +0530

    added image

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    
+    return <>
+        <h1>home</h1>
+        <Link to="/body" ><button>Products</button></Link>
+        </>
+}
+
+export default Home;
