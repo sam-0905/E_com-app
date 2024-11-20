343f0aceff0c633948e46770b0c1d10bc634ff5e
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-20 19:58:41 +0530

    added cart provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import "./style.css"
+import Cart from './pages/Cart';
+import { CartProvider } from './Context/Cart-Context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CartProvider>
     <Router>
     <App />
     </Router> 
+    </CartProvider>
   </React.StrictMode>,
 )
