3343362a999fb14073f03dc374da99efe2418ece
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-21 18:41:58 +0530

    changing the name in the reducer function

--- src/Context/Cart-Context.jsx
+++ src/Context/Cart-Context.jsx
@@ -1,10 +1,10 @@
 import { createContext, useContext, useReducer } from "react";
-import { cartFunc } from "../reducers/cart-reducer";
+import { cartReducer } from "../reducers/cart-reducer";
 
 const CartContext = createContext(" ")
 
 const CartProvider = ({children}) => {
-    const [state ,dispatch] = useReducer(cartFunc , {quantity: 0,
+    const [state ,dispatch] = useReducer(cartReducer , {quantity: 0,
         totalPrice: 0,
         items: [],}) 
     return (<CartContext.Provider value={{state,dispatch}}>{children}</CartContext.Provider>)
