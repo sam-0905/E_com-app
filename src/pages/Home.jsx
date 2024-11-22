487cae4e6273f3fc717785f6cbe2d9a05c173c57
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-22 19:26:51 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     
     return <>
+        <div>
         <h1>home</h1>
         <Link to="/body" ><button>Products</button></Link>
+        </div>
         </>
 }
 
