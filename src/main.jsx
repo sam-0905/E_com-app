3b2351cdaf45dba6ca9eb090e730268a1eb44ba6
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-24 19:26:40 +0530

    added a wish Provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import "./style.css"
 import { CartProvider } from './Context/Cart-Context';
+import { wishlistProvider } from './Context/wishlist-context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <wishlistProvider>
     <CartProvider>
     <Router>
     <App />
     </Router> 
     </CartProvider>
+    </wishlistProvider>
   </React.StrictMode>,
 )
