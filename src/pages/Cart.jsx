420d56803b4d1ce39dbf87c86e051aa61ceacb81
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-25 18:30:51 +0530

    updated cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -12,29 +12,39 @@ const Cart = () => {
   return (    
     <>
     <div className="cart">
+
       <div>
         {cartItems.length > 0 ? (
           cartItems.map(({ id, name, price, quantity ,image}) => (
           <div key={id} className="cart-card">
-              <img src={image}></img>
-              <h2>{name}</h2>
+              <div><img src={image}></img></div>
+              <div><h3>{name}</h3>
                <p className="price">Price: ${price}</p>
-               <p className="quantity">Quantity: {quantity}</p>
-               <hr />
+               <p className="quantity">Quantity: {quantity}</p></div>
+               <div className="cart-btn">
                <button className="button-32" onClick={()=> dispatch({type:"ADD_TO_CART", payload:{id,price}})}>+</button>
               <button className="button-32" onClick={() =>dispatch({ type:'REMOVE_FROM_CART',payload:{id,price}})}>-</button>
               <button className="button-32" onClick={() =>dispatch({ type:"DELETE_FROM_CART",payload:{id,price}})}><i class="fa fa-trash-o" style={{fontSize:"16px"}}></i>
               </button>
-              
+           </div>
           </div>
           ))
-        ) :( <h1> <img src={EmptyCart} alt="cart-icon" className="empty-cart animate__animated animate__fadeInLeft"  /><span className="text">Your cart is empty!</span></h1> )}
-        
+        ):( <div className="animate__animated animate__fadeInLeft">
+            <img src={EmptyCart} alt="cartImage" className="empty-cart"/>
+            <h1 className="text"> Your cart is empty</h1>
+        </div> )}
+              
       </div>
+      <div className="cart-order">
+                <h1>Order Summary</h1>
+                <h2>Quantity: {quantity}</h2>
+                <hr />
+                <h2>Total Price: {totalPrice}</h2>
+              </div>
     </div>
   </>       
-  )
-}
+  );
+};
 
   
 
