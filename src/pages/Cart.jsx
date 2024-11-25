0d9bd62954d04e73e9fb9f9a6173f9e99135e73b
Sam <118877482+sam-0905@users.noreply.github.com>
2024-11-25 13:18:16 +0530

    updated cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,41 +6,42 @@ import 'animate.css';
 
 
 const Cart = () => {
-  const { state, dispatch } = useCart();
+  const {state,dispatch} = useCart();
   const { cartItems,totalPrice,quantity } = state;
 
-  return (
+  return (    
     <>
     <div className="cart">
-        <div>
+      <div>
         {cartItems.length > 0 ? (
           cartItems.map(({ id, name, price, quantity ,image}) => (
-            <div key={id} className="cart-card">
-            <img src={image}></img>
-            <h2>{name}</h2>
-            <p className="price">Price: ${price}</p>
-            <p className="quantity">Quantity: {quantity}</p>
-            <hr />
-            <button className="button-32" onClick={()=> dispatch({type:"ADD_TO_CART", payload:{id,price}})}>+</button>
-            <button className="button-32" onClick={() =>dispatch({ type:'REMOVE_FROM_CART',payload:{id,price}})}>-</button>
-             <button
-              className="button-32"
-              onClick={() => dispatch({ type: "REMOVE_FROM_CART", payload:{id,price}})}>
-                {/* <i class="fa-sharp fa-solid fa-trash"></i> */}
-                <i class="fa fa-trash-o" style= {{fontSize:"16px"}}></i>
-
-             </button>
+          <div key={id} className="cart-card">
+              <img src={image}></img>
+              <h2>{name}</h2>
+               <p className="price">Price: ${price}</p>
+               <p className="quantity">Quantity: {quantity}</p>
+               <hr />
+               <button className="button-32" onClick={()=> dispatch({type:"ADD_TO_CART", payload:{id,price}})}>+</button>
+              <button className="button-32" onClick={() =>dispatch({ type:'REMOVE_FROM_CART',payload:{id,price}})}>-</button>
+              <button className="button-32" onClick={() =>dispatch({ type:"DELETE_FROM_CART",payload:{id,price}})}><i class="fa fa-trash-o" style={{fontSize:"16px"}}></i>
+              </button>
+              
           </div>
-        ))
-      ) : (
-       
-        <h1> <img src={EmptyCart} alt="cart-icon" className="empty-cart animate__animated animate__fadeInLeft"  /><span className="text">Your cart is empty!</span></h1>
-      )}
-
-     </div>
+          ))
+        ) :( <h1> <img src={EmptyCart} alt="cart-icon" className="empty-cart animate__animated animate__fadeInLeft"  /><span className="text">Your cart is empty!</span></h1> )}
+        
+      </div>
     </div>
-    </>
-  );
-};
+  </>       
+  )
+}
+
+  
 
 export default Cart;
+
+
+
+
+  
+   
